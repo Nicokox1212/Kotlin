66df8761085e107c8e74fd82337d1911b576091c
Nicokox1212 <nicola.allocca@infotech.edu.pl>
2024-09-10 09:48:31 +0200

    Zadanie praktyczne dla uczniów.pdf

---
+++ app/src/main/java/com/example/mojaaplikacjanicolaallocca/tutorialkotlin.kt
@@ -0,0 +1,79 @@
+package com.example.mojaaplikacjanicolaallocca
+
+//fun -> function
+fun main() {
+
+    //wypisywanie
+//    println("hello")
+//    println("xd")
+
+    //types
+    //int
+    //string
+    //char
+    //boolean true/false
+
+
+    // var -> variables
+//    var number : Int = 124
+//    var number2 = 125
+//    var text = "jakis tekst"
+//    var boolean = true // false
+//    var decimalvalue : Float = 123.33F
+//    println(number)
+
+    // val -> values
+//    val integerValue : Int = 123
+//    val integerVariables : Int = 123
+
+//    println("Uzytkownik")
+//    val keyboardValue = readLine()
+//
+//    println(keyboardValue)
+
+//var pointOfExam = 50
+//    when (pointOfExam){
+//        in 0..30 -> print("jedynka")
+//        in 31..50 -> print("dwojka")
+//        in 51..70 -> print("3")
+//        in 71..80 -> print("4")
+//        in 81..98 -> print("5")
+//    }
+//    var someValue : String = "some value"
+//
+//    when (someValue) {
+//        "value" -> "it is value"
+//    }
+//}
+//
+//fun newFunction(value: Any){
+//    when (value){
+//        "value" -> println("it is value")
+//        in 0..30 -> println("jedynka")
+//        10 -> println("it is 10")
+//        is String -> println("To jest string")
+//        is Int -> println("To jest string")
+//        true -> println("To jest true")
+//        else -> {
+//
+//        }
+//    }
+
+
+// 2. Zadanie praktyczne dla uczniów.pdf
+//        println("Podaj napis:")
+//        val keyboardValue = readLine()?:""
+//        val length = keyboardValue.length
+//        println("Twój napis ma $length znaków")
+
+
+//        println("Podaj napis:")
+//        val keyboardValue = readLine() ?: ""
+//
+//        when (keyboardValue) {
+//            "pomidor" -> println("pomidor")
+//            "truskawka" -> println("truskawka")
+//            else -> println("To nie jest ani truskawka ani pomidor")
+//    }
+
+}
